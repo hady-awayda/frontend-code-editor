b07cd366cc611b626d6ef0698fa49a4e8418764d
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-07-30 00:26:06 +0300

    Edited navbar styling for consistency

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -4,15 +4,17 @@ import "./style.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="home-link">
-        <Link to="/">Home</Link>
-      </div>
-      <div className="links">
-        <Link to="/admin">Admin</Link>
-        <Link to="/editor">Editor</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+      <div className="navbar-container">
+        <div className="home-link">
+          <Link to="/">Home</Link>
+        </div>
+        <div className="links">
+          <Link to="/admin">Admin</Link>
+          <Link to="/editor">Editor</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </div>
       </div>
     </nav>
   );
