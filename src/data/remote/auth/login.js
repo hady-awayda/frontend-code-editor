718bb956f1ed1f888272f5c90f7b0e0c49d6b241
hady-awayda <hady.awayda@gmail.com>
2024-07-30 22:40:50 +0300

    Added logging for baseURL to debug 404 error on deployement

--- src/data/remote/auth/login.js
+++ src/data/remote/auth/login.js
@@ -4,9 +4,10 @@ import storeToken from "../../../helpers/storeToken";
 const baseURL = import.meta.env.VITE_APP_DEPLOYMENT_BASE_URL;
 // const baseURL = import.meta.env.VITE_APP_BASE_URL;
 axios.defaults.baseURL = baseURL;
-console.log(baseURL);
 
 const login = async (email, password) => {
+  console.log(baseURL);
+
   const { data } = await axios.post("auth/login", {
     email,
     password,
