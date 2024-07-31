8496d6f947d81515e3f003d7ffdc4cf31255849b
hady-awayda <hady.awayda@gmail.com>
2024-07-31 17:22:28 +0300

    Added chats styling

--- src/components/Cards/Chat/index.jsx
+++ src/components/Cards/Chat/index.jsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+import fetchUserMessages from "../../../data/remote/messages/data";
+
 const Chat = ({ id }) => {
-  return <div className="bg-red-100 w-full">{id}</div>;
+  const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    const fetchChat = async () => {
+      const chat = await fetchUserMessages(id);
+      setChat(chat);
+      console.log(chat);
+    };
+
+    fetchChat();
+  }, [id]);
+
+  return (
+    <div className="bg-gray-100 w-full p-4 rounded-lg shadow-md">
+      <div className="overflow-y-auto h-96">
+        {chat.map((c) => {
+          console.log(c);
+          return (
+            <div
+              key={c.id}
+              className={`mb-4 p-3 rounded-lg max-w-xs ${
+                c.sender_id === id
+                  ? "bg-blue-500 text-white self-end"
+                  : "bg-white text-black self-start"
+              }`}
+            >
+              {c.message}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,29 +1,38 @@
 import "./style.css";
 import Conversation from "../../components/Cards/Conversation";
 import Chat from "../../components/Cards/Chat";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Profile = ({ user, conversations }) => {
-  const [id, setId] = useState("");
+  const [id, setId] = useState(null);
 
   const onConversationChange = (id) => {
     setId(id);
-    console.log(id);
   };
 
   return (
-    <div>
-      <h1 className="">Profile</h1>
-      <p>Name: {user?.name}</p>
-      <p>Email: {user?.email}</p>
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Profile</h1>
+      <div className="mb-4">
+        <p className="text-lg">
+          <strong>Name:</strong> {user?.name}
+        </p>
+        <p className="text-lg">
+          <strong>Email:</strong> {user?.email}
+        </p>
+      </div>
       <div className="flex w-full">
-        <div className="">
-          Conversations:
-          {conversations.map((c) => (
-            <Conversation key={c.id} {...{ ...c, onConversationChange }} />
-          ))}
+        <div className="w-1/3 pr-4">
+          <h2 className="text-xl font-semibold mb-2">Conversations:</h2>
+          <div className="">
+            {conversations.map((c) => (
+              <Conversation key={c.id} {...{ ...c, onConversationChange }} />
+            ))}
+          </div>
         </div>
-        <Chat {...{ id }} />
+        <div className="w-2/3 pl-4">
+          <Chat {...{ id }} />
+        </div>
       </div>
     </div>
   );
