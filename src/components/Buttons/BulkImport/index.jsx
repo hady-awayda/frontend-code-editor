622a2f347999b8cd3d3eff9fc3ce4445a4e505bc
hady-awayda <hady.awayda@gmail.com>
2024-08-01 01:41:50 +0300

    Implemented admin page with users data

---
+++ src/components/Buttons/BulkImport/index.jsx
@@ -0,0 +1,7 @@
+const BulkImport = () => {
+  return (
+    <button className="run-button h-16 w-32 font-semibold">Bulk Import</button>
+  );
+};
+
+export default BulkImport;
--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,6 +1,51 @@
-import "./style.css"
+import "./style.css";
+import getUsers from "../../data/remote/admin/read";
+import { useState, useEffect } from "react";
+import Import from "../../components/Buttons/BulkImport";
+
 const Admin = () => {
-  return <h1>This is the Admin Page</h1>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const data = await getUsers();
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div className="admin-page text-black">
+      <div className="flex justify-between items-center h-32 mx-12">
+        <h1 className="text-4xl font-bold text-white">Admin Dashboard</h1>
+        <Import />
+      </div>
+      <div className="user-list">
+        <h2>User List</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Created At</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{new Date(user.created_at).toLocaleString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
